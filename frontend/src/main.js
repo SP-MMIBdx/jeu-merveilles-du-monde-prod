786b2cd41eb1b24927f6ea83b71c275f7ec58dd0
SP-MMIBdx <stephen.pedder@mmibordeaux.com>
2026-03-06 10:31:56 +0100

    turn off dev mode

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -835,7 +835,7 @@ const config = {
         default: 'arcade',
         arcade: {
             gravity: { y: 600 },
-            debug: true
+            debug: false
         }
     },
     scene: { preload, create, update },
